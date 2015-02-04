e74a85352b709951fe2e711f3c17458ada912b9f
Daniel Romero <dromero@ymail.com>
2015-02-03 21:11:11 -0300

    Create FaceRecognition.py

---
+++ FaceRecognition.py
@@ -0,0 +1,38 @@
+import cv2
+import sys
+
+# Image to test
+imagePath = "faces.jpg"
+# pattern vector
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+
+# create a "virtual" image in black and white
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+)
+
+#  print result
+print "Found {0} faces".format(len(faces))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+# Draw image 
+cv2.imshow("Faces found", image)
+
+# Close key event
+cv2.waitKey(0)
